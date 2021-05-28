b072cd7dbdec58accba23b76998518dca907f3ac
Lucas Leone <lucasleonetatto@gmail.com>
2021-05-28 10:49:39 -0300

    estructura_secuencial terminada

--- estructura_secuencial/es19.py
+++ estructura_secuencial/es19.py
@@ -2,3 +2,10 @@
     Leer la base y altura de un rectángulo,
     calcular e imprimir la superficie
 '''
+
+base = float(input('La base del rectangulo es: '))
+altura = float(input('La altura del rectangulo es: '))
+
+superficie = base * altura
+
+print(f'La superficie del rectangulo es {superficie} cm2')
